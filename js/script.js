377f7ac807662453f6fd7b71a21de6b68a6c792f
Анатолий <anatolij@MacBook-Air-Anatolij.local>
2022-01-15 16:37:46 +0200

    Заканчил страници Новости. Начал страницу Услуги

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-$(document).ready(function()
-  {$('.slider').slick({
+$(document).ready(function(){
+
+  var $slider = $('.slider');
+  var $progressBar = $('.progress');
+  var $progressBarLabel = $( '.slider__label' );
+
+  $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    var calc = ( (nextSlide) / (slick.slideCount-1) ) * 100;
+
+    $progressBar
+      .css('background-size', calc + '% 100%')
+      .attr('aria-valuenow', calc );
+
+    $progressBarLabel.text( calc + '% completed' );
+  });
+  $('.slider').slick({
     arrows:false,
     infinite: true,
     slidesToShow:4,
