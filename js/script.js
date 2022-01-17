c198e232763a2ae47abbdea1a73c67a52689d769
Анатолий <anatolij@MacBook-Air-Anatolij.local>
2022-01-17 18:36:02 +0200

    Закончил страници Услуги, Наши работы, Отзывы , начал страницу О нас

--- js/script.js
+++ js/script.js
@@ -41,4 +41,78 @@ $(document).ready(function(){
         },
     ]
   });
+
+
+  $('.slider_reviews').slick({
+    arrows:true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: false,
+    variableWidth: true,
+    responsive: [
+     {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow:3,
+        }
+      },
+    ]
+  });
+
+    $("#contact_form").validate({
+    rules: {
+      first_name: {
+         required: true,// "required",
+         minlength: 2,
+         maxlength: 30,
+      },
+      last_name: {
+         required: true,// "required",
+         minlength: 2,
+         maxlength: 30,
+      },
+      phone: {
+         required: true,// "required",
+      },
+      email: {
+        email: true,
+        required: true
+      },
+      servise: {
+        required: true
+      }
+    },
+    messages: {
+      first_name: "Некорректно введены данные",
+      last_name: "Некорректно введены данные",
+      phone: "Некорректно введены данные",
+      email: "Некорректно введены данные",
+      servise: "Некорректно введены данные",
+    }
+  });
+
+  $("input[type='tel']").mask("+38 (099)-999-99-99");
+
+  $('#contact_form button').click(function(e){
+    e.preventDefault();
+    if ($('#contact_form').valid()){
+      location.href="#";
+      // me=$('#contact_form')
+      // $.ajax({
+      //   type: "POST",
+      //   data: me.serialize(),
+      //   success: function (data) {
+      //    me.find('.succ').slideDown(200)
+      //     setTimeout(function(){
+      //       me.find('.succ').slideUp(200)
+      //     },5000)
+      //     me.trigger('reset')
+      //   }
+      // })
+      // return false;
+    }
+  })
+
+
 })
